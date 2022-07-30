f277520066a0d2256d595cb6f05349b471d40854
mokyeah <mzmmzm2008@gmail.com>
2022-07-30 21:30:24 +0800

    huge update
    updated background, add 1 rhef and more

--- src/App.jsx
+++ src/App.jsx
@@ -1,75 +1,83 @@
 import React from 'react'
 import nuke from "./assets/nuke.jpg";
-import donald from "./assets/donald.jpg";
 import { Icon } from "@iconify/react";
 
 function App() {
   return (
-    <div className="bg-gradient-to-br from-green-300 flex justify-center items-center w-full h-screen ">
-      
-      <div className="rounded-xl overflow-hidden">
-      <div className="bg-white relative p-5 ">
-        <div className="flex">
-          <img src={nuke} className="w-64 md:w-96 " />
-          
-            <div className="text-md p-2 font-seimifold">cheap<strong> NUKE</strong>
-            <div className="text-md font-seimifold"><strong>$1000000</strong>
-              <div>
-                <div className='flex '>
-                  {Array(5)
-                    .fill(0)
-                    .map(() => (
-                      <div>
-                        <Icon
-                          icon="eva:star-fill"
-                          className="text-yellow-300 w-4 h-4 mt-1 ">
-                          
-                        </Icon>
-                        
-                      </div>
-                    ))}
-                    <div className='ml-2'><strong>(1)</strong></div>
+    <div className="bg-gradient-to-br bg-stone-300 flex justify-center items-center w-full h-screen ">
+
+      <div className="rounded-md overflow-hidden">
+        <div className="bg-white h-1/2 relative p-5 ">
+          <div className="">
+            <img src={nuke} className="w-128 md:w-96 " />
+
+            <div className="text-xl p-2 font-seimifold">cheap<strong> NUKE</strong>
+            <div className="text-s font-seimifold">The thing that Trump like alot</div>
+              <div className="text-md font-seimifold mt-2"><strong>$1000000</strong>
+                <div>
+                  <div className='flex '>
+                    {Array(5)
+                      .fill(0)
+                      .map(() => (
+                        <div>
+                          <Icon
+                            icon="eva:star-fill"
+                            className="text-yellow-300 w-4 h-4 mt-1 ">
+
+                          </Icon>
+
+                        </div>
+                      ))}
+                    <div className='-mt-1 text-s'> (1)</div>
+
+
+
+                    <div className='flex '></div>
+                  </div>
+                  <div className='flex flex-col mt-5  gap-2'>
+                    <div className='p-2  hover:animate-pulse transition-all bottom hover:scale-110 border-red-600 bg-gray-400 text-red-600 '>
+                      The Atomic Bomb
+                    </div>
+                    <div className='p-2  hover:animate-pulse transition-all bottom hover:scale-110 border-red-600 bg-gray-400 text-red-600'>
+                      Dirty Bombs
+                    </div>
+                    <div className='p-2 hover:animate-pulse transition-all bottom hover:scale-110 border-red-600  bg-gray-400 text-red-600'>
+                      Thermonuclear Weapons
+                    </div>
+                  
+                  
+                  <a
+                  
+                  className='p-2 rounded-md hover:animate-pulse mt-5 transition-all bottom hover:scale-110 border-sky-300 bottom bg-gray-600' href="https://www.youtube.com/watch?v=E5JPbpzZH-4">
+                    Add To Cart 
                     
-                    
-                    
-                    <div className='flex '></div>    
-                </div>
-                <div className='ml-2'><strong>TYPES</strong></div>
-                <div className='p-2 rounded-xl hover:animate-pulse transition-all bottom hover:border-2 border-red-600 bg-red-700  '>
-                The Atomic Bomb
-                </div>
-                <div className='p-2 rounded-xl hover:animate-pulse transition-all bottom hover:border-2 border-red-600 bg-orange-600 '>
-                Dirty Bombs
-                </div>
-                <div className='p-2 rounded-xl hover:animate-pulse transition-all bottom hover:border-2 border-red-600 bottom bg-red-300 '>
-                Thermonuclear Weapons
-                </div>
-                <div className='p-2 rounded-xl  right-1 bottom-3 w-min bg-sky-300 hover:animate-bounce hover:border-2 border-sky-500 transition-all'>
-                <Icon className="w-5 h-6 md:w-6 h-6" icon="fa-solid:cart-arrow-down" />
-                </div>
-                
+                  </a>
+                  
+                  </div>
                 </div>
               </div>
             </div>
           </div>
         </div>
-      </div>  
+      </div>
       
-      <div className="bg-red relative p-5 ">
-    
-        <img src={donald} className="w-32 bottom flex"></img>
-        <div className='flex'>
-          Trump: This Thing is Nice, I love it.
-          </div>    
-          
+
+      <div className='p-2 md:p-3 rounded-xl absolute right-3 top-3 w-min bg-stone-300' >
+        <Icon className="w-5 h-6 md:w-7 h-7" icon="fa-solid:cart-arrow-down" />
+      </div>
       
-        
-            
-     </div>       
+      <a
+      className="p-4 md:text-md bottom-3 font-seimifold absolute "><strong>Think before you buy. Why would you need such a thing? Drink some hot water and everything is going to be ok. </strong></a>
 
 
-</div>
+      </div>
     
+
+
+
+
+
+
 
 
   )
